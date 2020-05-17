8e9ddf8b3ced893124f1d7077895c5c5f8beb514
Haven <stackandslash@gmail.com>
2020-05-16 19:10:40 -0700

    both calls and updates set up, incl .each function, outline near completion

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 $(document).on("click", "a", function(event){
-
     console.log(event);
-    updateWeather(event.target.text)
+    updateWeather(event.target.text);
+})
+
+$("#searchbtn").on("click", function(event){
+    //console.log(event);
+    const citypass = $("#searchbox").val();
+    console.log(citypass);
+    updateWeather(citypass);
 })
 
 function updateWeather(cityname){
@@ -10,17 +16,37 @@ function updateWeather(cityname){
 var APIKey = "c0708fd314d4abadfb6401261f72c41f";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=" + APIKey;
 
-//target.text (for if the click was on one of the saved searches)
+//One call and update for the current weather situation in the specified area.
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
 
-    console.log(queryURL);
+  console.log(queryURL);
   console.log(response);
   $("#currenttemp").text("Temperature: " + response.main.temp.toFixed(1) + "°F");
   $("#headline").text(response.name + " TIME AND ICON");
   $("#currenthumidity").text("Humidity: " + response.main.humidity + "%");
   $("#currentwindspeed").text("Wind speed: " + response.wind.speed + " MPH");
+  //add the UV index
 }); 
+
+queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial&appid=" + APIKey;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(queryURL);
+      console.log(response);
+      $(".weatherblock").each(function(i){
+          const temp = response.list[i].main.temp;
+          console.log(this);
+          console.log(i);
+           $(this).children(".wbdate").text(""); //day+i, when we set moment up.
+           $(this).children(".wbtemp").text("Temperature: " + response.list[i].main.temp + "°F");
+           $(this).children(".wbicon").text(""); //icon goes here
+           $(this).children(".wbhumidity").text("Humidity: " + response.list[i].main.humidity + "%");  
+
+      })
+  }); 
 }
